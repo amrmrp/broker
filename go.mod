357c78c0e3a57bd0668b627727e6ac213582bfd6
amrmrp <amirreza.moghaddampoor@gmail.com>
2024-10-27 18:04:54 +0330

    update producer add newProducer and send message method

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module async-entity-fetcher
 
 go 1.23.1
+
+require github.com/wagslane/go-rabbitmq v0.14.2
+
+require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
--- pkg/rabbitmq/producer.go
+++ pkg/rabbitmq/producer.go
@@ -1,5 +1,42 @@
 package rabbitmq
 
-func produce(){
-	
+import (
+    "github.com/wagslane/go-rabbitmq"
+    "log"
+)
+
+// Producer ساختار تولیدکننده برای ارسال پیام‌ها
+type Producer struct {
+    publisher *rabbitmq.Publisher
+}
+
+// NewProducer یک تولیدکننده جدید ایجاد می‌کند
+func NewProducer(connString string) (*Producer, error) {
+    conn, err := rabbitmq.NewConn(connString, rabbitmq.WithConnectionOptionsLogging)
+    if err != nil {
+        return nil, err
+    }
+
+    publisher, err := rabbitmq.NewPublisher(conn, rabbitmq.WithPublisherOptionsLogging)
+    if err != nil {
+        return nil, err
+    }
+
+    return &Producer{publisher: publisher}, nil
+}
+
+// SendMessage پیام را به صف خاص ارسال می‌کند
+func (p *Producer) SendMessage(queue string, message string) error {
+    err := p.publisher.Publish(
+        []byte(message),
+        []string{queue},
+        rabbitmq.WithPublishOptionsContentType("text/plain"),
+        rabbitmq.WithPublishOptionsPersistentDelivery,
+    )
+    if err != nil {
+        log.Printf("خطا در ارسال پیام: %v", err)
+        return err
+    }
+    log.Printf("پیام ارسال شد: %s", message)
+    return nil
 }
