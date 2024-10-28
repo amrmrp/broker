d7d56efa35416e2c27a919835170acf66460def5
amrmrp <amirreza.moghaddampoor@gmail.com>
2024-10-28 21:17:48 +0330

    add func newrabbitconsumer logic

--- pkg/rabbitmq/consumer.go
+++ pkg/rabbitmq/consumer.go
@@ -1,6 +1,39 @@
 package rabbitmq
 
-func consume(){
+import (
+    "log"
+    "github.com/wagslane/go-rabbitmq"
+)
 
+
+func NewRabbitConsumer(queueName string,routeKey string,exchangeName string) {
+	conn, err := rabbitmq.NewConn(
+		"amqp://guest:guest@localhost",
+		rabbitmq.WithConnectionOptionsLogging,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	
+	consumer, err := rabbitmq.NewConsumer(
+		conn,
+		queueName,
+		rabbitmq.WithConsumerOptionsRoutingKey(routeKey),
+		rabbitmq.WithConsumerOptionsExchangeName(exchangeName),
+		rabbitmq.WithConsumerOptionsExchangeDeclare,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer consumer.Close()
+	
+	err = consumer.Run(func(d rabbitmq.Delivery) rabbitmq.Action {
+		log.Printf("consumed: %v", string(d.Body))
+		// rabbitmq.Ack, rabbitmq.NackDiscard, rabbitmq.NackRequeue
+		return rabbitmq.Ack
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-
