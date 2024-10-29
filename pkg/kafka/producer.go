2a2df7a4d7602a4b0aae94e6335617d3a04878b6
amrmrp <amirreza.moghaddampoor@gmail.com>
2024-10-29 21:54:11 +0330

    addd kafka pckage and create kafka producer

--- pkg/kafka/producer.go
+++ pkg/kafka/producer.go
@@ -1,5 +1,31 @@
 package kafka
 
-func produce(){
+import (
+    "github.com/segmentio/kafka-go"
+	"context"
+	"log"
 
+)
+
+func CreateKafkaProducer(brokerAddress,topic string,message []byte)error{
+
+	writer := kafka.Writer{
+		Addr: kafka.TCP(brokerAddress),
+		Topic: topic,
+		Balancer: &kafka.LeastBytes{},
+	}
+	defer writer.Close()
+
+	err  := writer.WriteMessages(context.Background(),
+		kafka.Message{
+			Key: []byte("Key"),
+			Value: message,
+		},
+)
+
+	if err != nil {
+		log.Printf("Failed to write message to Kafka: %v",err)
+	}
+
+	return err
 }
