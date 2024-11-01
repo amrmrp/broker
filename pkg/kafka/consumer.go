84592a682fec4cd829be666ae41e915e36d8008b
amrmrp <amirreza.moghaddampoor@gmail.com>
2024-11-01 12:46:28 +0330

    add start kafka consumer method

--- pkg/kafka/consumer.go
+++ pkg/kafka/consumer.go
@@ -1,5 +1,28 @@
 package kafka
 
-func consume(){
+import(
+	"context"
+	"log"
+    "github.com/segmentio/kafka-go"
+)
 
+
+func StartKafkaConsumer(brokerAddress,topic string){
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{brokerAddress},
+		Topic: topic,
+		GroupID: "consumer-group-1",
+	})
+
+	defer reader.Close()
+
+	for{
+		msg, err := reader.ReadMessage(context.Background())
+
+		if err != nil {
+			log.Printf("Error reading message: %v",err)
+			continue
+		}
+		log.Printf("Received message at %v %s",msg.Time,string(msg.Value))
+	}
 }
