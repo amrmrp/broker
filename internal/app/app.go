0f23c1bd753104c38d7cdc289716445cdc1bc70c
amrmrp <amirreza.moghaddampoor@gmail.com>
2024-11-15 14:10:47 +0330

    inital message to kafka broker

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,13 +1,47 @@
 package app
 
 import (
-    "fmt"
-    "async-entity-fetcher/pkg/example"
+	//  "fmt"
+	//   "async-entity-fetcher/pkg/example"
+	"context"
+	"log"
+	"time"
+
+	"github.com/segmentio/kafka-go"
 )
 
 // Start initializes the application
 func Start() {
-    fmt.Println("Application is running...")
-    result := example.SayHello("World")
-    fmt.Println(result)
+
+	// to produce messages
+	topic := "my-topic"
+	partition := 0
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	_, err = conn.WriteMessages(
+		kafka.Message{Value: []byte("one!")},
+		kafka.Message{Value: []byte("two!")},
+		kafka.Message{Value: []byte("three!")},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+
+
+    log.Println("success produce 😊")
+
+	/*
+	   fmt.Println("Application is running...")
+	   result := example.SayHello("World")
+	   fmt.Println(result)
+	*/
 }
