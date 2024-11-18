ac49e2cc5004f50be6094dbbcfe6faeef1e5bd4c
amrmrp <amirreza.moghaddampoor@gmail.com>
2024-11-18 19:32:18 +0330

    add message struct and get agument routekey and topic and partition

--- pkg/kafka/producer.go
+++ pkg/kafka/producer.go
@@ -1,31 +1,74 @@
 package kafka
 
 import (
-    "github.com/segmentio/kafka-go"
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"time"
 
+	"github.com/google/uuid"
+	"github.com/segmentio/kafka-go"
 )
 
-func CreateKafkaProducer(brokerAddress,topic string,message []byte)error{
+type Data struct{}
 
-	writer := kafka.Writer{
-		Addr: kafka.TCP(brokerAddress),
-		Topic: topic,
-		Balancer: &kafka.LeastBytes{},
+type MyMessage struct {
+	ID      string              `json:"id"`
+	Command string              `json:"command"`
+	Data    map[string][]string `json:"data"`
+	Time    time.Time           `json:"time"`
+}
+
+func CreateKafkaProducer(message map[string][]string, routeKey string, topic string, partition int) {
+	/*
+		-------------------------------------------------------------------------
+		| to produce messages and initial message structure
+		-------------------------------------------------------------------------
+	*/
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
 	}
-	defer writer.Close()
 
-	err  := writer.WriteMessages(context.Background(),
+	messages := MyMessage{
+		ID:      uuid.New().String(),
+		Command: routeKey,
+		Data:    message,
+		Time:    time.Now(),
+	}
+
+	/*
+		-------------------------------------------------------------------------
+		| Serialize the object to JSON
+		-------------------------------------------------------------------------
+	*/
+	messagesSerialize, err := json.Marshal(messages)
+	if err != nil {
+		log.Fatalf("failed to serialize message: %v", err)
+	}
+
+	/*
+		-------------------------------------------------------------------------
+		| new connection and produce job
+		-------------------------------------------------------------------------
+	*/
+	conn.SetWriteDeadline(time.Now().Add(3 * time.Second))
+	_, err = conn.WriteMessages(
 		kafka.Message{
-			Key: []byte("Key"),
-			Value: message,
+			Key:   []byte(routeKey),
+			Value: messagesSerialize,
 		},
-)
+	)
 
 	if err != nil {
-		log.Printf("Failed to write message to Kafka: %v",err)
+		log.Fatal("failed to write messages:", err)
 	}
 
-	return err
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+
+	fmt.Println("Starting HelloWorld Application...")
+	log.Println("success produce 😊")
 }
