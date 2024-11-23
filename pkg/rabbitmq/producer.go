351e94ece0e25141c632c631bcb7fb6857e1349f
amrmrp <amirreza.moghaddampoor@gmail.com>
2024-11-23 21:52:56 +0330

    add rabbit producer and binding message structure

--- pkg/rabbitmq/producer.go
+++ pkg/rabbitmq/producer.go
@@ -1,20 +1,46 @@
 package rabbitmq
 
 import (
+	"encoding/json"
 	"log"
-
+	"time"
+	"github.com/google/uuid"
 	"github.com/wagslane/go-rabbitmq"
 )
 
-func CreateRabbitProducer(message map[string][]string, routeKey string, topic string, partition int) {
+type MyMessage struct {
+	ID      string              `json:"id"`
+	Command string              `json:"command"`
+	Data    map[string][]string `json:"data"`
+	Time    time.Time           `json:"time"`
+}
+
+func CreateRabbitProducer(message map[string][]string, routeKey string, topic string) {
 	conn, err := rabbitmq.NewConn(
-		"amqp://guest:guest@localhost",
+		"amqp://admin:admin@localhost",
 		rabbitmq.WithConnectionOptionsLogging,
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
+
+	messages := MyMessage{
+		ID:      uuid.New().String(),
+		Command: routeKey,
+		Data:    message,
+		Time:    time.Now(),
+	}
+
+	/*
+		-------------------------------------------------------------------------
+		| Serialize the object to JSON
+		-------------------------------------------------------------------------
+	*/
+	messagesSerialize, err := json.Marshal(messages)
+	if err != nil {
+		log.Fatalf("failed to serialize message: %v", err)
+	}
 
 	publisher, err := rabbitmq.NewPublisher(
 		conn,
@@ -28,10 +54,10 @@ func CreateRabbitProducer(message map[string][]string, routeKey string, topic st
 	defer publisher.Close()
 
 	err = publisher.Publish(
-		[]byte("hello, world"),
-		[]string{"my_routing_key"},
+		[]byte(messagesSerialize),
+		[]string{routeKey},
 		rabbitmq.WithPublishOptionsContentType("application/json"),
-		rabbitmq.WithPublishOptionsExchange("events"),
+		rabbitmq.WithPublishOptionsExchange(topic),
 	)
 	if err != nil {
 		log.Println(err)
