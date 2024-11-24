1a8187e3ad9862251203c918b868dd51c5169f46
amrmrp <amirreza.moghaddampoor@gmail.com>
2024-11-24 21:58:21 +0330

    test application producer

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,5 +1,7 @@
 package app
 
+import "async-entity-fetcher/pkg/rabbitmq"
+
 //  "fmt"
 //   "async-entity-fetcher/pkg/example"
 
@@ -7,13 +9,17 @@ package app
 func Start() {
 
 	/*
-		data := make(map[string][]string)
-		data["entity1"] = []string{"item1", "item2", "item3"}
-		data["entity2"] = []string{"item1", "item2", "item3"}
-		data["entity3"] = []string{"item1", "item2", "item3"}
-		rabbitmq.CreateRabbitProducer(data,"invoice.cmd.created","amq.topic")
+	data := make(map[string][]string)
+	data["entity1"] = []string{"item1", "item2", "item3"}
+	data["entity2"] = []string{"item1", "item2", "item3"}
+	data["entity3"] = []string{"item1", "item2", "item3"}
+	rabbitmq.CreateRabbitProducer(data,"invoice.cmd.created","master")
 	*/
 
+    
+//	rabbitmq.NewRabbitConsumer("service1", "invoice.cmd.created", "master")
+
+	
 	/*
 		data := make(map[string][]string)
 		data["entity1"] = []string{"item1", "item2", "item3"}
