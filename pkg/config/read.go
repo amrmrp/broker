d36bcf7fab3c4c6a50f3a7a40f7d0504ded3907e
amrmrp <amirreza.moghaddampoor@gmail.com>
2024-11-29 22:12:20 +0330

    add config setter and getter

--- pkg/config/read.go
+++ pkg/config/read.go
@@ -1,6 +1,32 @@
 package config
 
-func readConfigs(){
-	
-	
+import (
+	"fmt"
+	"log"
+	"os"
+	"gopkg.in/yaml.v3"
+)
+
+type Configs struct{ Config interface{} }
+
+func (config *Configs) SetConfigs(location string, structure interface{}) {
+
+	// Read YAML file
+	file, err := os.ReadFile(location)
+	if err != nil {
+		log.Fatalf("Error reading YAML file: %v", err)
+	}
+
+	// Unmarshal into the Config struct
+	err = yaml.Unmarshal(file, structure)
+	if err != nil {
+		log.Fatalf("Error unmarshaling YAML content: %v", err)
+	}
+
+	config.Config = structure
+}
+
+func (config *Configs) GetConfigs() {
+	// Access and print RabbitMQ URL
+	fmt.Printf("configs: %s\n", config)
 }
