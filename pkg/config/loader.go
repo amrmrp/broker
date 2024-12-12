a4ac2b4130c720c690a3af04aa23529f5917e8c6
amrmrp <amirreza.moghaddampoor@gmail.com>
2024-12-12 15:25:59 +0330

    dynamic initial config with looping

--- pkg/config/loader.go
+++ pkg/config/loader.go
@@ -1,20 +1,29 @@
 package config
 
 import (
+	"log"
 	"os"
+
 	"gopkg.in/yaml.v3"
 )
 
 func LoadConfig(path string) (*Configs, error) {
 	file, err := os.ReadFile(path)
-	if(err != nil){
-		return nil,err
+	if err != nil {
+		return nil, err
 	}
 
 	var config Configs
-	err = yaml.Unmarshal(file,&config)
-	if err != nil{
-		return nil, err
+	configMap := map[string]interface{}{
+		"Kafka":    &config.Kafka,
+		"RabbitMQ": &config.RabbitMQ,
+	}
+
+	for name, target := range configMap {
+		err = yaml.Unmarshal(file, target)
+		if err != nil {
+			log.Fatalf("Error unmarshaling %s config: %v", name, err)
+		}
 	}
 
 	return &config, nil
